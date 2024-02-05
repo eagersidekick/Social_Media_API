6a5e2ad2757b7a6566cbed8839a6f1f4febd7dc3
Heather Crawford <heather.crawford.ela@gmail.com>
2024-02-04 18:00:42 -0600

    edited schemas to contain virtuals. Removed Reactions as its own file and added the schema to Thoughts instead.

--- Models/Thought.js
+++ Models/Thought.js
@@ -1,36 +1,74 @@
-const { Schema, model } = require("mongoose");
-const reactionSchema = require("./Reaction");
-const moment= require('moment');
+const { Schema, model, Types } = require('mongoose');
+//moment 
+const moment = require('moment')
 
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: "Whoops! Looks like your Thought was empty",
-      minlength: 1,
-      maxlength: 280,
+//the schema for the reaction schema ONLY
+const reactionSchema = new Schema (
+    {
+       reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+       },
+       reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+       },
+       username: {
+        type: String,
+        required: true,
+       },
+       createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+       },
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-  }
-);
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
 
-thoughtSchema.virtual("reactionCount").get(function () {
-  return this.reactions.length;
-});
+// the schema for thoughts
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
 
-module.exports = thoughtSchema;
+
+// virtual that retrieves the length of the thought's reactions array field on query 
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
--- Models/User.js
+++ Models/User.js
@@ -1,5 +1,6 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
+//the schema for users
 const userSchema = new Schema(
   {
     username: {
@@ -9,33 +10,39 @@ const userSchema = new Schema(
       trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [
-          [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
-          "Please use a valid email address",
-        ],
-      },
-      thoughts: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "Thought",
-        },
-      ],
-      friends: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-        },
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please use a valid email.",
       ],
     },
-    {
-      toJSON: {
-        virtuals: true,
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
       },
-      id: false,
-    }
-  );
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-module.exports = userSchema;
+//virtual that retrieves the length of the user's friends array field on query.
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
